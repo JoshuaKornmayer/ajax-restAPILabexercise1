4a5807978876f7aa01f3bbc1ec1d1cb1d07ac0c1
JoshuaKornmayer <jbkornmayer80@gmail.com>
2020-04-15 20:19:28 -0400

    commit of exercise 1 again updated

--- ajaxrestapilab.js
+++ ajaxrestapilab.js
@@ -33,8 +33,44 @@ $('#id10').click(function(p2){
 //2
 
 //3
-$('#comment12').click(function(p2){
-	$.get('https://my-json-server.typicode.com/zachhall/WIN2020_AjaxPromises/comments/12', function(p2){
+$('#comment12').click(function(x){
+	$.get('https://my-json-server.typicode.com/zachhall/WIN2020_AjaxPromises/comments', {id: 12} , function(x){
+		console.log(x);
+			list1.empty();
+			x.forEach(function(y){
+			var myList = $('<li></li>');
+			myList.text(JSON.stringify(y));
+			list1.append(myList);
+		})
+		})
+
+	})
+
+//4 Get all the posts from user with id of 2
+$('#userid12').click(function(x){
+	$.get('https://my-json-server.typicode.com/zachhall/WIN2020_AjaxPromises/user/2/posts', function(x){
+		console.log(x);
+		list1.empty();
+		x.forEach(function(y){
+			if(y.userID == 2){
+			var myList = $('<li></li>');
+			myList.text(JSON.stringify(y));
+			list1.append(myList);
+			} else {
+				console.log("no two here");
+			} 
+		})
+	})
+})
+
+//5 Create a new post and log the id generated for it by the server
+$('#newPost').click(function(p2){
+	$.post('https://my-json-server.typicode.com/zachhall/WIN2020_AjaxPromises/posts', {
+		userID: 85,
+		id: 21,
+		title: "New Post",
+		body: "This is my New Post Body"
+	} , function(p2){
 		console.log(p2);
 			list1.empty();
 			var myList2 = $('<li></li>');
@@ -43,6 +79,63 @@ $('#comment12').click(function(p2){
 		})
 
 	})
+
+//6 Replace the post with id of 12 and render the responseJSON
+$('#replacePost').click(function(p2){
+	$.ajax({
+		method: 'PUT',
+		url: 'https://my-json-server.typicode.com/zachhall/WIN2020_AjaxPromises/posts/12',
+		data: {
+			userID: 85,
+			id: 21,
+			title: "Updated New Post",
+			Body: "This is the updated new post" 
+		}, complete: function(response){
+			console.log(response.responseJSON);
+		}
+	}, function(p2){
+			console.log(p2);
+				list1.empty();
+				var myList2 = $('<li></li>');
+				myList2.text(JSON.stringify(p2));
+				list1.append(myList2);
+			}
+	)
+})
+
+
+
+//7 Update the title of post with id of 12 and render responseJSON
+$('#updatePost').click(function(p2){
+	$.ajax({
+		method: 'PATCH',
+		url: 'https://my-json-server.typicode.com/zachhall/WIN2020_AjaxPromises/posts/12',
+		data: {
+		
+			title: "Patched",
+			 
+		}, complete: function(response){
+			console.log(response.responseJSON);
+		}
+	}, function(p2){
+			console.log(p2);
+				list1.empty();
+				var myList2 = $('<li></li>');
+				myList2.text(JSON.stringify(p2));
+				list1.append(myList2);
+			}
+	)
+})
+
+//8 Delete the post with id of 12 and render a success message
+
+//9 Display a list of posts
+
+//10 When the user clicks on a post, display all the comments from that post
+
+//11 Display a link back to all posts
+
+
 
 
 
